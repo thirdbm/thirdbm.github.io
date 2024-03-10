3ef2d94d4c7395fd9821d341789d56bba6e9bf58
BossMimic <katzbalgerr@gmail.com>
2024-03-11 03:14:03 +0900

    update

---
+++ common.js
@@ -0,0 +1,10 @@
+let header = 
+'<div class="mainMenu">' +
+	'<a href="https://thirdbm.github.io">Home</a> | ' +
+    '<a href="./archive.html">Archive</a> | ' +
+    '<a href="./games.html">Games</a> | ' +
+    '<a href="./about.html">About</a>' +
+'</div>' +
+'<div class="mainSpace"></div>';
+	
+document.getElementById("commonHeader").innerHTML = header;
